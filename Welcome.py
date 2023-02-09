c3a50691e617eaf7006207375c2bc25949a38a33
Trey L <90742156+UptoCode99@users.noreply.github.com>
2023-02-09 15:53:19 -0700

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,9 +1,14 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(
     page_title="Welcome",
     page_icon="👋",
 )
 
-st.write("Welcome To Price App")
+st.header("Welcome To Price App")
+
+st.subheader('Instructions')
+
+st.write('In this application we have infomation about selling crops in Idaho. Each tab to your left will have different tools to help you evaulate when and where to sell')
 
