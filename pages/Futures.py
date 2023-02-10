4a9e2c865fc97fab79b6330cb54c33fd4911f33b
Trey L <90742156+UptoCode99@users.noreply.github.com>
2023-02-10 12:29:00 -0700

    Update Futures.py

--- pages/Futures.py
+++ pages/Futures.py
@@ -5,4 +5,6 @@ st.set_page_config(page_title="Futures Charts", page_icon="📈")
 
 st.write('This page will have the same infomation from the powerbi page')
 
+img = "https://github.com/Lusk27/app_display/blob/main/Data/chart.png?raw=true"
 
+st.image(img)
