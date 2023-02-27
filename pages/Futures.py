76da6bc85f4403b823d88f50658bb8c9107b8c20
Trey L <90742156+UptoCode99@users.noreply.github.com>
2023-02-27 16:23:21 -0700

    Update Futures.py

--- pages/Futures.py
+++ pages/Futures.py
@@ -7,7 +7,8 @@ st.set_page_config(page_title="Futures Charts", page_icon="📈")
 st.write('This page will have the same infomation from the powerbi page')
 
 
-df =pd.read_csv('/Users/luskenterprises/consulting/app_display/Data/test.csv')
+df =pd.read_csv('Lusk27/app_display/Data/test.csv')
+
 
 st.table(df)
 
