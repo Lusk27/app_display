01050d15718c34628bf2f0ba135c5cafee006e3c
Trey L <90742156+UptoCode99@users.noreply.github.com>
2023-02-27 16:19:31 -0700

    Update Futures.py

--- pages/Futures.py
+++ pages/Futures.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import pandas as pd
+import plotly.figure_factory as ff
 
 st.set_page_config(page_title="Futures Charts", page_icon="📈")
 
 st.write('This page will have the same infomation from the powerbi page')
 
-img = "https://github.com/Lusk27/app_display/blob/main/Data/chart.png?raw=true"
 
-st.image(img)
+df =pd.read_csv('/Users/luskenterprises/consulting/app_display/Data/test.csv')
+
+st.table(df)
+
+#st.bar_chart(data=df, x=Location )
