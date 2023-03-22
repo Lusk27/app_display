c6f2df98649aa29a4ffd86fe33a2535c91a3a3c5
Trey L <90742156+UptoCode99@users.noreply.github.com>
2023-03-21 23:53:38 -0600

    Added wheat prices

--- pages/Wheat_Prices.py
+++ pages/Wheat_Prices.py
@@ -4,3 +4,8 @@ import pandas as pd
 st.set_page_config(page_title="Wheat Charts", page_icon="🌾")
 
 st.write("This will have the wheat infomation")
+
+CITY = st.selectbox(
+    'Select a City',
+    ('Rexburg / Ririe','Idaho Falls','Blackfoot / Pocatello','Grace / Soda Springs','Burley / Rupert','Meridian',
+'Nezperce / Craigmont','Nampa / Weiser','Twin Falls / Buhl / Jerome / Wendell','Moscow / Genesee'))
